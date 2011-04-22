118961cf9322f5bac427580d80d0bcd4ac4fd772
Jens Nyberg <jens.nyberg@gmail.com>
2011-04-22 23:56:30 +0200

    removed unneccessary configuration of root window

--- src/window.c
+++ src/window.c
@@ -12,18 +12,14 @@ static void halo_window_init_root(struct halo *halo)
 
     halo->root = XRootWindow(halo->display, halo->screen);
 
+    XSelectInput(halo->display, halo->root, SubstructureRedirectMask | SubstructureNotifyMask | StructureNotifyMask | PropertyChangeMask);
+
     halo->atom_net[0] = XInternAtom(halo->display, "_NET_SUPPORTED", 0);
     halo->atom_net[1] = XInternAtom(halo->display, "_NET_WM_NAME", 0);
     halo->atom_net[2] = XInternAtom(halo->display, "_NET_WM_STATE", 0);
     halo->atom_net[3] = XInternAtom(halo->display, "_NET_WM_STATE_FULLSCREEN", 0);
 
     XChangeProperty(halo->display, halo->root, halo->atom_net[0], XA_ATOM, 32, PropModeReplace, (unsigned char *)halo->atom_net, 4);
-
-    XSetWindowAttributes attributes;
-    attributes.event_mask = SubstructureRedirectMask | SubstructureNotifyMask | StructureNotifyMask | PropertyChangeMask;
-
-    XChangeWindowAttributes(halo->display, halo->root, CWEventMask, &attributes);
-    XSelectInput(halo->display, halo->root, attributes.event_mask);
 
 //    XCompositeRedirectSubwindows(halo->display, halo->root, CompositeRedirectAutomatic);
 
