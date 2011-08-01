f6f9aefbf8eefd9b23e1f26e4bc4a6fdf5f081cb
jezze <jens.nyberg@gmail.com>
2011-08-01 02:02:09 +0200

    small cleanup

--- src/window.c
+++ src/window.c
@@ -12,17 +12,14 @@ static void xurfaced_window_init_root(struct xurfaced_display_backend *backend)
 {
 
     backend->root = XRootWindow(backend->display, backend->screen);
-
-    XSelectInput(backend->display, backend->root, SubstructureRedirectMask | SubstructureNotifyMask | StructureNotifyMask | PropertyChangeMask);
-
     backend->atom_net[0] = XInternAtom(backend->display, "_NET_SUPPORTED", 0);
     backend->atom_net[1] = XInternAtom(backend->display, "_NET_WM_NAME", 0);
     backend->atom_net[2] = XInternAtom(backend->display, "_NET_WM_STATE", 0);
     backend->atom_net[3] = XInternAtom(backend->display, "_NET_WM_STATE_FULLSCREEN", 0);
 
+    XSelectInput(backend->display, backend->root, SubstructureRedirectMask | SubstructureNotifyMask | StructureNotifyMask | PropertyChangeMask);
     XChangeProperty(backend->display, backend->root, backend->atom_net[0], XA_ATOM, 32, PropModeReplace, (unsigned char *)backend->atom_net, 4);
     XGrabKey(backend->display, XKeysymToKeycode(backend->display, XK_c), Mod1Mask | ShiftMask, backend->root, 1, GrabModeAsync, GrabModeAsync);
-    XGrabKey(backend->display, XKeysymToKeycode(backend->display, XK_q), Mod1Mask | ShiftMask, backend->root, 1, GrabModeAsync, GrabModeAsync);
     XGrabKey(backend->display, XKeysymToKeycode(backend->display, XK_Escape), Mod1Mask, backend->root, 1, GrabModeAsync, GrabModeAsync);
     XGrabKey(backend->display, XKeysymToKeycode(backend->display, XK_Tab), Mod1Mask, backend->root, 1, GrabModeAsync, GrabModeAsync);
     XCompositeRedirectSubwindows(backend->display, backend->root, CompositeRedirectAutomatic);
@@ -33,18 +30,11 @@ static void xurfaced_window_init_root(struct xurfaced_display_backend *backend)
 static void xurfaced_window_init_main(struct xurfaced_display_backend *backend)
 {
 
-    backend->main = XCreateSimpleWindow(backend->display, backend->root, 0, 0, backend->width, backend->height, 0, XBlackPixel(backend->display, backend->screen), XBlackPixel(backend->display, backend->screen));
-
-    XWindowAttributes wa;
-
-    XGetWindowAttributes(backend->display, backend->main, &wa);
-
-    XRenderPictFormat *format = XRenderFindVisualFormat(backend->display, wa.visual);
-
     XRenderPictureAttributes pa;
     pa.subwindow_mode = IncludeInferiors;
 
-    backend->mainPicture = XRenderCreatePicture(backend->display, backend->main, format, CPSubwindowMode, &pa); 
+    backend->main = XCreateSimpleWindow(backend->display, backend->root, 0, 0, backend->width, backend->height, 0, XBlackPixel(backend->display, backend->screen), XBlackPixel(backend->display, backend->screen));
+    backend->mainPicture = XRenderCreatePicture(backend->display, backend->main, XRenderFindVisualFormat(backend->display, backend->visual), CPSubwindowMode, &pa); 
 
     //XShapeSelectInput(backend->display, backend->main, ShapeNotifyMask);
     XSelectInput(backend->display, backend->main, ButtonPressMask | KeyPressMask);
